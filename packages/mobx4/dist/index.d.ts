6e75956b0c5157355a0099eeb8fcdf2f97d89008
欧阳斌 <oyb@ouyangbindeiMac.local>
2023-06-06 12:09:32 +0800

    rm dist

--- packages/mobx4/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { action, computed, observable } from './mobx.js';
-export { runInAction, configure, toJS } from './mobx.js';
-export { isObservableArray, isObservableObject, isObservableSet, isObservableMap } from './mobx.js';
-export { observer, react, reactor, render } from './decorators.js';
